82194048294dfb00c4b5f96c1a1313c2522893e8
RahulKurup <formyrahul@gmail.com>
2015-11-14 19:08:02 +0530

    Comments

--- src/com/search/sort/oneCounter.java
+++ src/com/search/sort/oneCounter.java
@@ -1,3 +1,18 @@
+/*Given a binary array sorted in non-increasing order, count the number of 1’s in it.
+
+Examples:
+
+Input: arr[] = {1, 1, 0, 0, 0, 0, 0}
+Output: 2
+
+Input: arr[] = {1, 1, 1, 1, 1, 1, 1}
+Output: 7
+
+Input: arr[] = {0, 0, 0, 0, 0, 0, 0}
+Output: 0*/
+
+
+
 package com.search.sort;
 
 public class oneCounter {
