9e25783ed99cc372fd89f5c72aa849ffd9d93dda
RahulKurup <formyrahul@gmail.com>
2015-11-28 11:24:24 +0530

    Binary Conversion
    Binary Conversion

--- src/com/rahul/mathcoding/BinaryToDecimalConveter.java
+++ src/com/rahul/mathcoding/BinaryToDecimalConveter.java
@@ -1,15 +1,11 @@
 package com.rahul.mathcoding;
 
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.TreeMap;
 
 public class BinaryToDecimalConveter {
 
     public static void main(String[] args) {
 
         System.out.println(binaryToDecimal1("101"));
-        histogram("abcdaaaaaaaaaaaaabbbbbbbbbcccccccccccccccccccccccccddddddddddddefghi");
     }
 
 
@@ -32,28 +28,5 @@ public class BinaryToDecimalConveter {
             binary = binary/10;
         }   
         return decimal;
-    }
-
-
-    /**
-     * @param input
-     */
-    public static void histogram(String input) {
-
-        final Map<Character, Integer> map = new TreeMap<Character, Integer>();
-        for (int i = 0; i < input.length(); i++) {
-            if (! map.containsKey(input.charAt(i))) {
-                map.put(input.charAt(i), 0);
-            }
-            map.put(input.charAt(i), map.get(input.charAt(i)) + 1);       
-        }
-
-        for (final Entry<Character, Integer> entry : map.entrySet()) {
-            System.out.print(entry.getKey());
-            for (int i = 0 ; i < entry.getValue() || i == 79; i ++ ) {
-                System.out.print("*");
-            }
-            System.out.print("\n");
-        }
     }
 }
