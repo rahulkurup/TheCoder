83c0b9db2697749e0759efaa1e4c16b6bee210f2
RahulKurup <formyrahul@gmail.com>
2015-11-30 22:47:15 +0530

    Insertion Sort
    Insertion Sort

---
+++ src/com/search/sort/InsertionSort.java
@@ -0,0 +1,31 @@
+package com.search.sort;
+
+public class InsertionSort {
+
+    public static void main(String[] args) {
+
+        int [] array = {2,7 ,8, 45, 2,0,49,6};
+        Insertionsort(array);
+
+        for(int a : array) {
+            System.out.println(a);
+        }
+    }
+
+    public static void Insertionsort(int[] array) {
+
+        for(int i = 1; i < array.length; i++) {
+            int number = array[i];
+            int j = i-1;            
+            while(j >=0 && array[j] > number) {
+                array[j+1] = array[j];
+                j--;
+            }
+            if(j == i-1) {
+
+            } else {
+                array[j+1]  = number;               
+            }
+        }   
+    }
+}
